5dc98079d68838c834c23d4316b5765a98c9c6a4
abasheryassin <abasheryassin@gmail.com>
2022-11-28 12:38:37 -0600

    add current date

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,6 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// the code isn't run until the browser has finished rendering all the elements in the html.
+var dateDisplayEl = $('#currentDay');
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +20,9 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  function displayTime() {
+    var date = dayjs().format('MMM DD, YYYY');
+    dateDisplayEl.text(date);
+  }
+  displayTime();
 });
